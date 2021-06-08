e1abc3d7e2668833fd425416c0c5dae60e3d401e
armalaster <fixais@bk.ru>
2021-06-08 11:41:33 +0500

    MyList с заполненным листом

---
+++ ConsoleApp8/MyList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+	class MyList<T>
+	{
+		public void Add(T value)
+		{
+			Node<T> ddd = new Node<T>();
+			ddd.value = value;
+			if (firstElement == null)
+			{
+				firstElement = ddd;
+			}
+			else
+			{
+				GetLast().next = ddd;
+			}
+		}
+		Node<T> firstElement;
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			return last;
+		}
+		public void Print()
+		{
+			Node<T> last = firstElement;
+			Console.WriteLine("Записанные данные в List последним элементом в списке, поставленный в начале:");
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+			}
+		}
+		public void listPush(T v1)
+		{
+			Node<T> b = new Node<T>();
+			b.value = v1;
+			if (firstElement != null)
+			{
+				firstElement.prev = b;
+				b.next = firstElement;
+				firstElement = b;
+				Console.WriteLine("Последий элемент из списка это: " + v1);
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
